53feed0aeee3c46588ec2fc0f709e6aceb16ef56
Motorhead1991 <motor-head-1991@hotmail.com>
2014-08-17 13:45:04 -0700

    Update extract-files and setup-makefiles

--- extract-files.sh
+++ extract-files.sh
@@ -3,7 +3,7 @@
 VENDOR=lge
 DEVICE=l35g
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+BASE=../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
