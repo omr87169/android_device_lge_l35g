8c4e8bddf128ad94f0f5c2958f6a90b4662e1b08
devhost <you@example.com>
2014-08-29 19:42:29 -0400

    This is apparently a cortex-a5 arch variant and not a cortex-a7 as I originally thought.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := cortex-a5
 
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_CPU_SMP := true
